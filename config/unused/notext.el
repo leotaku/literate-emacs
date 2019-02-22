727d0c4f54b72945a2db63844747de5a0de0a79c
LeOtaku <leo.gaskin@brg-feldkirchen.at>
2019-02-22 21:23:56 +0100

    Remove old files from git tracking, and update README and init

--- config/unused/notext.el
+++
@@ -1,9 +0,0 @@
-;; packages for use with things other than text files
-
-(use-package nov
-  :straight t
-  :defer t
-  :mode ("\\.epub\\'" . nov-mode)
-  :hook (nov-mode . (lambda () (face-remap-add-relative 'variable-pitch
-                                                        :family "Liberation Serif"
-                                                        :height 1.5))))
--- config/unused/perf.el
+++
@@ -1,8 +0,0 @@
-;; packages for better emacs performance
-
-(use-package vlf
-  :straight t
-  :defer t
-  :config
-  (require 'vlf-setup)
-  (setq vlf-application 'dont-ask))
